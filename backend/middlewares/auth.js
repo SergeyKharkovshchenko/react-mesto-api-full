56da00140e57ca24dfb33d05ab8edc152470102a
Sergey Kharkovshchenko <kharkovchenko@gmail.com>
2023-01-10 13:58:38 +0300

    загрузил ПР15 после проверки 1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -13,7 +13,8 @@ const checkAuth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    // payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (e) {
     next(new UnauthorizedError('Необходима авторизация'));
     return;
