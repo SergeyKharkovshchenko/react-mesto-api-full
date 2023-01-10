f09a17c36646ac9184a434f7c4d7a0ab24e019d9
Sergey Kharkovshchenko <kharkovchenko@gmail.com>
2023-01-10 20:48:16 +0300

    загрузил ПР15 после проверки 1

--- backend/app.js
+++ backend/app.js
@@ -21,22 +21,22 @@ console.log(process.env.NODE_ENV); // production
 
 const app = express();
 
-const allowedCors = [
-  'http://sergey-kh.nomoredomains.club/',
-  'https://sergey-kh.nomoredomains.club/',
-  'http//localhost:3000',
-  'https://localhost:3000',
-];
+// const allowedCors = [
+//   'http://sergey-kh.nomoredomains.club/',
+//   'https://sergey-kh.nomoredomains.club/',
+//   'http//localhost:3000',
+//   'https://localhost:3000',
+// ];
 
-const corsOptions = {
-  origin: allowedCors,
-  optionsSuccessStatus: 200,
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: allowedCors,
+//   optionsSuccessStatus: 200,
+//   credentials: true,
+// };
 
 app.use(requestLogger);
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 // app.use(cors());
 
 app.use(cookieParser());
